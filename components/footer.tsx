9ea3e4ecf1c85c38223f44f39b942e452bbe2df4
m1erla <116915007+m1erla@users.noreply.github.com>
2023-12-10 22:27:56 +0300

    finised some components and re-checked if its workes well

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
         &copy; 2028 Furkan. All rights reserved.
       </small>
--- components/intro.tsx
+++ components/intro.tsx
@@ -43,20 +43,6 @@ export default function Intro() {
             "
             />
           </motion.div>
-
-          <motion.span
-            className="absolute bottom-0 right-0 text-4x1 "
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
-            }}
-          >
-            👋
-          </motion.span>
         </div>
       </div>
       <motion.h1
@@ -64,7 +50,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Furkan Karakus.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Furkan.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">1 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
